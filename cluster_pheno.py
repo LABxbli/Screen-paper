a9a3944d32f8eef2d580624f527563d42d519eaa
LABxbli <163239403+LABxbli@users.noreply.github.com>
2024-11-13 14:50:54 +0800

    cluster_pheno.py
    for mutants phenotypical cluster

--- cluster_pheno.py
+++ cluster_pheno.py
@@ -10,8 +10,7 @@ def log_norm(df):
 data = pd.read_csv("/screen/D_plate_phenotype/data.csv",index_col=0, header=0)
 print(data.describe())
 print(data.head())
-#data = data[~data.index.duplicated()]
-#More than 75% of the data multiples are below 2, but there are still cases where the data is 0, so log2(n+1) is used to process the data
+
 
 
 spe = data.pop('group')
@@ -29,13 +28,9 @@ row_colors = spe.map(lut)
 
 clab = ['G3V2']
 
-#Draw
 g = sns.clustermap(
     data=df_log_norm,
     row_colors=row_colors,
-    #row_cluster=False,
-    #col_cluster=False,
-    #z_score=1,
     cmap='bwr',
     vmax=1.28,
     vmin=-1.28,
@@ -47,10 +42,9 @@ g.figure.set_size_inches(12,20)
 reordered_labels = df_log_norm.index[g.dendrogram_row.reordered_ind].tolist()
 use_labels = clab
 use_ticks = [reordered_labels.index(label) + .5 for label in use_labels]
-#Obtain the reset ticks data
+
 g.ax_heatmap.set(yticks=use_ticks, yticklabels=use_labels)
 
-#Figure legend
 for lable in spe.unique():
     g.ax_row_dendrogram.bar(0, 0, color=lut[lable],
                             label=lable, linewidth=0
@@ -59,10 +53,8 @@ g.ax_row_dendrogram.legend(loc="center", ncol=1)
 
 use_labels = clab
 use_ticks = [reordered_labels.index(label) + .5 for label in use_labels]
-#Obtain the reset ticks data
 
 g.ax_heatmap.set(yticks=use_ticks, yticklabels=use_labels)
-#Reset the y-label of the cluster
 
 re_lab = df_log_norm.columns[g.dendrogram_col.reordered_ind].tolist()
 
